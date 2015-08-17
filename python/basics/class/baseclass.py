e5548e22c21e21ce8aa064c939b5c431c60efea6
100credit_u <baixuexue123@hotmail.com>
2015-08-17 16:46:11 +0800

    new.py

---
+++ python/basics/class/baseclass.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''test'''
+
+__author__ = 'baixue'
+
+
+class Base(object):
+    pass
+
+
+setattr(Base, 'name', 'baseclass')
+setattr(Base, 'version', 1.0)
---
+++ python/basics/class/baseclasstest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''doc string'''
+
+
+from baseclass import Base
+
+
+print Base.name
+print Base.version
