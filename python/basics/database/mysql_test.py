291d3ff4659479276f634f57b02358bada67ccbc
100credit_u <baixuexue123@hotmail.com>
2015-08-27 14:56:29 +0800

    redis “

--- python/basics/database/mysql_test.py
+++ python/basics/database/mysql_test.py
@@ -10,71 +10,72 @@ import MySQLdb
 
 
 try:
-    conn=MySQLdb.connect(host='localhost', user='root', passwd='35535', db='test', port=3306)
+    conn=MySQLdb.connect(host='localhost', user='root', passwd='35535', db='easyspider', port=3306)
     cur=conn.cursor()
-    cur.execute('select * from user')
+    cur.execute('select * from car58')
+    print cur.fetchall()
     cur.close()
     conn.close()
 except MySQLdb.Error, e:
     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
 
 
-try:
-    conn=MySQLdb.connect(host='localhost',user='root',passwd='root',port=3306)
-    cur=conn.cursor()
+# try:
+#     conn=MySQLdb.connect(host='localhost',user='root',passwd='root',port=3306)
+#     cur=conn.cursor()
 
-    cur.execute('create database if not exists python')
-    conn.select_db('python')
-    cur.execute('create table test(id int,info varchar(20))')
+#     cur.execute('create database if not exists python')
+#     conn.select_db('python')
+#     cur.execute('create table test(id int,info varchar(20))')
 
-    value=[1,'hi rollen']
-    cur.execute('insert into test values(%s,%s)',value)
+#     value=[1,'hi rollen']
+#     cur.execute('insert into test values(%s,%s)',value)
 
-    values=[]
-    for i in range(20):
-        values.append((i,'hi rollen'+str(i)))
+#     values=[]
+#     for i in range(20):
+#         values.append((i,'hi rollen'+str(i)))
 
-    cur.executemany('insert into test values(%s,%s)', values)
+#     cur.executemany('insert into test values(%s,%s)', values)
  
-    cur.execute('update test set info="I am rollen" where id=3')
+#     cur.execute('update test set info="I am rollen" where id=3')
 
-    conn.commit()  # 这句来提交事务，要不然不能真正的插入数据
-    cur.close()
-    conn.close()
+#     conn.commit()  # 这句来提交事务，要不然不能真正的插入数据
+#     cur.close()
+#     conn.close()
 
-except MySQLdb.Error,e:
-    print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+# except MySQLdb.Error,e:
+#     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
 
 
-try:
-    conn=MySQLdb.connect(host='localhost',user='root',passwd='root',port=3306)
-    cur=conn.cursor()
+# try:
+#     conn=MySQLdb.connect(host='localhost',user='root',passwd='root',port=3306)
+#     cur=conn.cursor()
      
-    conn.select_db('python')
+#     conn.select_db('python')
  
-    count=cur.execute('select * from test')
-    print 'there has %s rows record' % count
+#     count=cur.execute('select * from test')
+#     print 'there has %s rows record' % count
  
-    result=cur.fetchone()
-    print result
-    print 'ID: %s info %s' % result
+#     result=cur.fetchone()
+#     print result
+#     print 'ID: %s info %s' % result
  
-    results=cur.fetchmany(5)
-    for r in results:
-        print r
+#     results=cur.fetchmany(5)
+#     for r in results:
+#         print r
  
-    print '=='*10
-    cur.scroll(0,mode='absolute')
+#     print '=='*10
+#     cur.scroll(0,mode='absolute')
  
-    results=cur.fetchall()
-    for r in results:
-        print r[1]
+#     results=cur.fetchall()
+#     for r in results:
+#         print r[1]
 
-    conn.commit()
-    cur.close()
-    conn.close()
+#     conn.commit()
+#     cur.close()
+#     conn.close()
  
-except MySQLdb.Error,e:
-    print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+# except MySQLdb.Error,e:
+#     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
 
 
