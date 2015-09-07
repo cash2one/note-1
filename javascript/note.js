040df93e984b0e5cab3d4d29bd825076d198189b
bai_uhome <baixuexue123@hotmail.com>
2015-09-07 21:33:30 +0800

    note.js

--- javascript/note.js
+++ javascript/note.js
@@ -290,3 +290,147 @@ a = 'ABC'; // a变为字符串
 // 启用strict模式的方法是在JavaScript代码的第一行写上:
 
 // 为了避免这一缺陷，所有的JavaScript代码都应该使用strict模式
+
+
+
+/******  Map和Set  ******/
+// JavaScript的默认对象表示方式{}可以视为其他语言中的Map或Dictionary的数据结构，即一组键值对
+// 但是JavaScript的对象有个小问题，就是键必须是字符串
+// 但实际上Number或者其他数据类型作为键也是非常合理的
+
+// 最新的ES6规范引入了新的数据类型Map。要测试你的浏览器是否支持ES6规范，请执行以下代码:
+'use strict';
+var m = new Map();
+var s = new Set();
+alert('你的浏览器支持Map和Set！');
+
+
+var m = new Map([['Michael', 95], ['Bob', 75], ['Tracy', 85]]);
+m.get('Michael'); // 95
+
+// 初始化Map需要一个二维数组，或者直接初始化一个空Map。Map具有以下方法:
+var m = new Map(); // 空Map
+m.set('Adam', 67); // 添加新的key-value
+m.set('Bob', 59);
+m.has('Adam'); // 是否存在key 'Adam': true
+m.get('Adam'); // 67
+m.delete('Adam'); // 删除key 'Adam'
+m.get('Adam'); // undefined
+
+var m = new Map();
+m.set('Adam', 67);
+m.set('Adam', 88);
+m.get('Adam'); // 88
+
+
+// Set和Map类似，也是一组key的集合，但不存储value。由于key不能重复，所以，在Set中，没有重复的key
+
+// 要创建一个Set，需要提供一个Array作为输入，或者直接创建一个空Set
+var s1 = new Set(); // 空Set
+var s2 = new Set([1, 2, 3]); // 含1, 2, 3
+
+// 重复元素在Set中自动被过滤
+var s = new Set([1, 2, 3, 3, '3']);
+s; // Set {1, 2, 3, "3"}
+
+// 通过add(key)方法可以添加元素到Set中，可以重复添加，但不会有效果:
+s.add(4);
+s;  // {1, 2, 3, 4}
+s.add(4);
+s;  // {1, 2, 3, 4}
+
+var s = new Set([1, 2, 3]);
+s; // Set {1, 2, 3}
+s.delete(3);
+s; // Set {1, 2}
+
+
+
+/******  iterable  ******/
+// Array、Map和Set都属于iterable类型
+// 具有iterable类型的集合可以通过新的for ... of循环来遍历
+// for ... of循环是ES6引入的新的语法
+'use strict';
+var a = [1, 2, 3];
+for (var x of a) {
+}
+alert('你的浏览器支持for ... of');
+
+
+var a = ['A', 'B', 'C'];
+var s = new Set(['A', 'B', 'C']);
+var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
+for (var x of a) { // 遍历Array
+    alert(x);
+}
+for (var x of s) { // 遍历Set
+    alert(x);
+}
+for (var x of m) { // 遍历Map
+    alert(x[0] + '=' + x[1]);
+}
+
+
+var a = ['A', 'B', 'C'];
+a.name = 'Hello';
+for (var x in a) {
+    alert(x); // '0', '1', '2', 'name'
+}
+
+var a = ['A', 'B', 'C'];
+a.name = 'Hello';
+for (var x of a) {
+    alert(x); 'A', 'B', 'C'
+}
+
+
+// 更好的方式是直接使用iterable内置的forEach方法
+var a = ['A', 'B', 'C'];
+a.forEach(function (element, index, array) {
+    // element: 指向当前元素的值
+    // index: 指向当前索引
+    // array: 指向Array对象本身
+    alert(element);
+});
+
+// forEach()方法是ES5.1标准引入的，你需要测试浏览器是否支持
+
+// Set与Array类似，但Set没有索引，因此回调函数最多两个参数
+var s = new Set(['A', 'B', 'C']);
+s.forEach(function (element, set) {
+    alert(element);
+});
+
+
+// Map
+var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
+m.forEach(function (value, key, map) {
+    alert(value);
+});
+
+
+// 如果对某些参数不感兴趣，由于JavaScript的函数调用不要求参数必须一致，因此可以忽略它们
+var a = ['A', 'B', 'C'];
+a.forEach(function (element) {
+    alert(element);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
