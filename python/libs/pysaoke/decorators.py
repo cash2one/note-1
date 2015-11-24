2f425ca82ec87cb9dcb0585dd7401c05feaf4f17
100credit_u <baixuexue123@hotmail.com>
2015-11-24 17:18:47 +0800

    add decorators.py

---
+++ python/libs/pysaoke/decorators.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+""" decorator """
+
+import time
+import traceback
+import functools
+
+
+def clock(ms=True):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            st, st_c = time.time(), time.clock()
+            ret = func(*args, **kwargs)
+            print 'run time: %.1fms' % ((time.time()-st)*1000) if ms else 'run time: %.4fs' % (time.time()-st)
+            print 'cpu time: %.1fms' % ((time.clock()-st_c)*1000) if ms else 'cpu time: %.4fs' % (time.clock()-st_c)
+            return ret
+        return wrapper
+    return decorator
+
+
+def catch(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception:
+            print '>>>>>>>>' + wrapper.__name__ + '>>>>>>>>'
+            traceback.print_exc()
+    return wrapper
+
+
+def trace_run(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print '>>>>>>>> function *%s* is called :: %s >>>>>>>>' % \
+              (wrapper.__name__, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+        return func(*args, **kwargs)
+    return wrapper
--- python/libs/pysaoke/utils.py
+++ python/libs/pysaoke/utils.py
@@ -1,51 +1,59 @@
 # -*- coding: utf-8 -*-
 
-"""toolkits"""
-
-__author__ = 'Pysaoke'
-
+""" toolkit
+@author: Pysaoke
+Created on 15-11-24
+"""
 
 import os
-import sys
 import itertools
+import urllib
+import urlparse
+import datetime
 
 
-def get_basename(path):
-    '''获取一个文件的名字，不包括扩展名'''
+def basename(path):
+    """ 获取一个文件的名字,不包括扩展名 """
     return os.path.splitext(os.path.basename(path))[0]
 
 
-def getfile(dirs, ext=None):
-    '''获取给目录内的所有文件(不包括子目录里的文件), 可以指定文件扩展名来获取指定类型的文件'''
-    if not os.path.isdir(dirs):
+def getfile(dir, ext=None):
+    """ 获取给定目录内的所有文件(不包括子目录里的文件),可以指定文件扩展名来获取指定类型的文件
+    :param dir: 目录绝对路径
+    :param ext: 扩展名
+    :return: list 文件列表
+    """
+    if not os.path.isdir(dir):
         return
-    files =  os.listdir(dirs)
-    files =  filter(lambda x:os.path.isfile(os.path.join(dirs, x)), files)
+    files = filter(lambda x: os.path.isfile(os.path.join(dir, x)), os.listdir(dir))
     if ext:
-        files = filter(lambda x:os.path.splitext(x)[1]==ext, files)
+        files = filter(lambda x: os.path.splitext(x)[1]==ext, files)
     return files
 
 
 def listget(lst, idx, default=None):
-    '''若列表中某索引存在则返回之'''
-    if -len(lst) <= idx < len(lst):return lst[idx]
-    else:return default
+    """ 若列表中某索引存在则返回之 """
+    if -len(lst) <= idx < len(lst):
+        return lst[idx]
+    else:
+        return default
 
 
 def list_get(lst, idx, default=None):
-    '如果传递的索引绝大多数都是有效索引，那么用这个函数'
-    try:return lst(idx)
-    except IndexError:return default
+    """ 如果传递的索引绝大多数都是有效索引,那么用这个函数 """
+    try:
+        return lst(idx)
+    except IndexError:
+        return default
 
 
 def par_two(a, b, padding_item=None):
-    '''并行地循环两个可迭代对象, 短的用padding_item补充'''
+    """ 并行地循环两个可迭代对象,短的用padding_item补充 """
     a, b = iter(a), iter(b)
-    # 首先，对两者用izip处理，直达其中一个耗尽
+    # 首先,对两者用izip处理,直达其中一个耗尽
     for x in itertools.izip(a, b):
         yield x
-    # 下面两个循环最多只有一个能够执行
-    # 因为此时a或b至少有一个是耗尽的
+    # 下面两个循环最多只有一个能够执行,因为此时a或b至少有一个是耗尽的
     for x in a:
         yield x, padding_item
     for x in b:
@@ -53,7 +61,7 @@ def par_two(a, b, padding_item=None):
 
 
 def par_loop(padding_item, *sequences):
-    '''并行地循环多个可迭代对象, 短的用padding_item补充'''
+    """ 并行地循环多个可迭代对象,短的用padding_item补充 """
     iterators = map(iter, sequences)
     num_remaining = len(iterators)
     result = [padding_item] * num_remaining
@@ -70,9 +78,10 @@ def par_loop(padding_item, *sequences):
 
 
 def smallmerge(*sequences):
-    '''合并有序序列'''
+    """ 合并有序序列 """
     result = []
-    for subseq in sequences:result.extend(subseq)
+    for subseq in sequences:
+        result.extend(subseq)
     return result
 
 
@@ -80,16 +89,14 @@ def smallmerge(*sequences):
 def sub_dict_select(x_dict, keys):
     return dict([(k, x_dict[k]) for k in keys if k in x_dict])
 
+
 # 获取字典的子集,有默认值
 def sub_dict(x_dict, keys, default=None):
     return dict([(k, x_dict.get(k, default)) for k in keys])
 
 
-import datetime
-
-
 def last_month(multiple=1):
-    """上几个月的日期"""
+    """ 上几个月的日期 """
     today = datetime.date.today()
     year, month = divmod(multiple, 12)
     offset = today.month - month
@@ -103,19 +110,14 @@ def gen_date():
     return map(last_month, range(6))
 
 
-import urllib
-import urlparse
-from urlparse import urljoin
-
-
 def qs(url):
-    """获取url中的查询参数"""
+    """ 获取url中的查询参数 """
     query = urlparse.urlparse(url).query
     return dict([(k, v[0]) for k, v in urlparse.parse_qs(query).items()])
 
 
 def add_qs(url, params=None, **kwargs):
-    """添加url参数"""
+    """ 添加url参数 """
     if not params:
         data = urllib.urlencode(kwargs)
     else:
