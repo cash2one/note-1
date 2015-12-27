8bac4a608ed2374a103e4d9dad03aa83e6c9eadd
baixue <baixuexue123@hotmail.com>
2015-12-27 15:34:59 +0800

    csv DictReader DictWriter

--- python/basics/file/csv/test.py
+++ python/basics/file/csv/test.py
@@ -1,25 +1,44 @@
 import csv, codecs
 
 
+# csv.writer
 fobj = open('my.csv', 'wb')
 fobj.write(codecs.BOM_UTF8)
 writer = csv.writer(fobj)
 writer.writerow(['Column1', 'Column2', 'Column3'])
 lines = [range(3) for i in range(5)]
+
 for line in lines:
     writer.writerow(line)
 
-
-# fobj = open(filename, 'wb')
-# fobj.write(codecs.BOM_UTF8)
-# writer = csv.writer(fobj)
-# writer.writerow(headers)
+fobj.close()
 
 
-csvfile = file('csv_test.csv', 'rb')
+# csv.reader
+csvfile = file('my.csv', 'rb')
 reader = csv.reader(csvfile)
 
 for line in reader:
     print line
 
 csvfile.close()
+
+
+# csv.DictReader
+for d in csv.DictReader(open('my.csv', 'rb')):
+	print d
+
+
+# csv.DictWriter
+fobj = open('my1.csv', 'wb')
+fobj.write(codecs.BOM_UTF8)
+FIELDS = ['Column1', 'Column2', 'Column3']
+writer = csv.DictWriter(fobj, fieldnames=FIELDS)
+lines = [dict(zip(FIELDS, range(3))) for i in range(5)]
+
+lines.insert(0, dict(zip(FIELDS, FIELDS)))
+
+for line in lines:
+    writer.writerow(line)
+
+fobj.close()
