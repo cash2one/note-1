63fcf49c7f7192953bfb08e9f2299135a09cabf3
bxf_100_u <baixuexue123@hotmail.com>
2016-06-14 10:28:11 +0800

    ssl patch

--- problems/ssl/patch.py
+++ problems/ssl/patch.py
@@ -3,33 +3,38 @@
 
 """ssl版本问题的解决办法"""
 
+import ssl
+import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
-import ssl
 
 
 class MyAdapter(HTTPAdapter):
 
     def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = PoolManager(num_pools=connections,
-                                       maxsize=maxsize,
-                                       block=block,
-                                       ssl_version=ssl.PROTOCOL_TLSv1)
- 
+        self.poolmanager = PoolManager(
+                num_pools=connections,
+                maxsize=maxsize,
+                block=block,
+                ssl_version=ssl.PROTOCOL_TLSv1
+            )
+
 s = requests.Session()
 s.mount('https://', MyAdapter())  # 所有的https连接都用ssl.PROTOCOL_SSLV3去连接
 s.get('https://xxx.com')
 
 
-
+# **********************************************************************************
 # custom HTTPS opener, banner's oracle 10g server supports SSLv3 only
+# **********************************************************************************
+
 import httplib, ssl, urllib2, socket
 
 
 class HTTPSConnectionV3(httplib.HTTPSConnection):
     def __init__(self, *args, **kwargs):
         httplib.HTTPSConnection.__init__(self, *args, **kwargs)
-         
+
     def connect(self):
         sock = socket.create_connection((self.host, self.port), self.timeout)
         if self._tunnel_host:
@@ -37,7 +42,7 @@ class HTTPSConnectionV3(httplib.HTTPSConnection):
             self._tunnel()
         try:
             self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file, ssl_version=ssl.PROTOCOL_SSLv3)
-        except ssl.SSLError, e:
+        except ssl.SSLError:
             print("Trying SSLv3.")
             self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file, ssl_version=ssl.PROTOCOL_SSLv23)
 
@@ -45,7 +50,7 @@ class HTTPSConnectionV3(httplib.HTTPSConnection):
 class HTTPSHandlerV3(urllib2.HTTPSHandler):
     def https_open(self, req):
         return self.do_open(HTTPSConnectionV3, req)
-# install opener
+
 urllib2.install_opener(urllib2.build_opener(HTTPSHandlerV3()))
 
 
