925afa6a1f193a369d993940238a821f763941e4
bxf_100_u <baixuexue123@hotmail.com>
2016-08-11 10:19:06 +0800

    actor

--- python/concurrency/gevent/actor.py
+++ python/concurrency/gevent/actor.py
@@ -5,13 +5,13 @@ from gevent import monkey; monkey.patch_all()
 from gevent.queue import Queue, Empty
 
 
-class Actor(gevent.Greenlet):
+class Actor1(gevent.Greenlet):
 
     def __init__(self, name, interval):
         self.inbox = Queue()
         self.name = name
         self.interval = interval
-        super(Actor, self).__init__()
+        super(Actor1, self).__init__()
 
     def shutdown(self):
         self.running = False
@@ -29,14 +29,13 @@ class Actor(gevent.Greenlet):
             gevent.sleep(self.interval)
 
 
-class Actor2(gevent.Greenlet):
+class Actor(gevent.Greenlet):
 
-    def __init__(self, name, interval):
+    def __init__(self, interval):
         self.inbox = Queue()
-        self.running = True
-        self.name = name
-        self.interval = interval
-        super(Actor2, self).__init__()
+        self._interval = interval
+        self._timeout = None
+        super(Actor, self).__init__()
 
     def work(self):
         """
@@ -47,22 +46,39 @@ class Actor2(gevent.Greenlet):
     def _run(self):
         while True:
             try:
-                msg = self.inbox.get(block=True, timeout=self.interval)
+                msg = self.inbox.get(block=True, timeout=self._timeout)
                 if msg == 'stop':
+                    self._timeout = None
+                elif msg == 'start':
+                    self._timeout = self._interval
+                elif msg == 'shutdown':
                     break
             except Empty:
                 self.work()
+            gevent.sleep(0)
 
-    def shutdown(self):
+    def start(self):
+        self.inbox.put('start')
+        return super(Actor, self).start()
+
+    def stop(self):
         self.inbox.put('stop')
 
+    def shutdown(self):
+        self.inbox.put('shutdown')
 
-class Pinger(Actor2):
+
+class Pinger(Actor):
+    
+    def __init__(self, name, interval):
+        self.name = name
+        super(Pinger, self).__init__(interval)
+
     def work(self):
         print(self.name)
 
 
-class Ponger(Actor2):
+class Ponger(Pinger):
     def work(self):
         print(self.name)
 
@@ -73,7 +89,20 @@ pong = Ponger('BBBBBBB', 1)
 ping.start()
 pong.start()
 
-gevent.sleep(10)
+ping.start()
+pong.start()
+
+gevent.sleep(5)
+
+ping.stop()
+pong.stop()
+
+gevent.sleep(5)
+
+ping.start()
+pong.start()
+
+gevent.sleep(5)
 
 ping.shutdown()
 pong.shutdown()
