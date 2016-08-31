6df7f1430704279ecc7282f9d443323d046f7389
bxf_100_u <baixuexue123@hotmail.com>
2016-08-31 11:46:52 +0800

    expect habse shell 通过

--- python/others/test_pexpect.py
+++ python/others/test_pexpect.py
@@ -1,39 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import re
 import time
 import subprocess
-import pexpect
 
+import pexpect
 import ptyprocess
 
-p = ptyprocess.PtyProcess.spawn(['redis-cli'])
-time.sleep(1)
-print p.read(1024)
-p.write('set baixue 666\n')
-time.sleep(1)
-print p.read(1024)
-time.sleep(1)
-print p.read(1024)
+# p = ptyprocess.PtyProcess.spawn(['redis-cli'])
+# time.sleep(1)
+# print p.read(1024)
+# p.write('set baixue 666\n')
+# time.sleep(1)
+# print p.read(1024)
+# time.sleep(1)
+# print p.read(1024)
 
 
-# child = pexpect.spawn('redis-cli')
-# i = child.expect(['127.0.0.1:6379>', pexpect.EOF, pexpect.TIMEOUT], timeout=10)
-# print i
-# if i == 0:
-#     print 'success'
-# else:
-#     print 'fail'
+child = pexpect.spawn('hbase', ['shell'])
+i = child.expect(['hbase\(main\).+?>', pexpect.EOF, pexpect.TIMEOUT], timeout=10)
+print i
+if i == 0:
+    print 'success'
+else:
+    print 'fail'
+
+child.sendline("status 'simple'")
+i = child.expect(['hbase\(main\).+?>', pexpect.EOF, pexpect.TIMEOUT], timeout=10)
+print i
+if i == 0:
+    print 'before:'
+    print child.before
+    print 'success'
+else:
+    print 'fail'
+
+child.sendline('quit')
+i = child.expect([pexpect.EOF, pexpect.TIMEOUT], timeout=3)
+print i
+if i == 0:
+    print 'quit success'
+else:
+    print 'quit fail'
+
+child.close()
+
+# pattern = re.compile(r'hbase\(main\).+?>')
 #
-# child.sendline('set baixue 4')
-# i = child.expect(['127.0.0.1:6379>', pexpect.EOF, pexpect.TIMEOUT], timeout=10)
-# print i
-# if i == 0:
-#     print 'before:'
-#     print child.before
-#     print 'success'
-# else:
-#     print 'fail'
-#
-# child.sendeof()
-# child.close()
+# print re.match(pattern, 'hbase(main):001:10>')
