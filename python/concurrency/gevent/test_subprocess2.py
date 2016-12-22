4b8edcb4b96c772a16eab0755c12d1b1c980306e
bxf_100_u <baixuexue123@hotmail.com>
2016-12-22 19:45:54 +0800

    subprocess

--- python/concurrency/gevent/test_subprocess2.py
+++ python/concurrency/gevent/test_subprocess2.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-
-"""
-Python标准库子进程测试
-如果用标准库启动子进程, gevent停止运行后, 子进程开启的程序仍然运行.
-如果用gevent.subprocess运行子进程,那么gevent结束后,子进程开启的程序也停止
-"""
-
 import gevent
 from subprocess import Popen, PIPE
 
