b090a212cc434e2b3429d59cc1f3f8c406acb7f9
bxf_100_u <baixuexue123@hotmail.com>
2017-03-09 15:25:08 +0800

    unicode trans

--- python/basics/chn_unicode/test.py
+++ python/basics/chn_unicode/test.py
@@ -14,7 +14,16 @@ print u'\u767d\u96ea'
 
 print unichr(0x767d)
 print unichr(0x767d).encode('utf-8')
-
 print unichr(int('fd9b', 16)).encode('utf-8')
 
-print unicode('\u767d\u96ea')
+
+print u'\u767d\u96ea'
+
+print u'\u767d\u96ea'.encode('utf-8')
+print u'白雪'.encode('utf-8')
+
+print u'\u767d\u96ea' == u'白雪'  # True
+
+print u'白雪'.encode("unicode_escape")
+
+print '\u767d\u96ea'.decode('unicode_escape') == u'白雪'  # True
